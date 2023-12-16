40da4b3de77d6d968e45505af185e3a630c3c71d
FP22FD <112474910+FP22FD@users.noreply.github.com>
2023-12-16 01:13:01 +0100

    new hosting

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 export const settings = {
   // wp_baseurl: "https://flower-power.local",
-  wp_baseurl: "https://fp22fd-wp-ca.no/",
+  wp_baseurl: "https://wordpress-ca.receitasdeferdi.com/",
   woo: {
     consumer_key: "ck_88ddfac510ec65daab9d4a260eeaf18b92174d3b",
     consumer_secret: "cs_e47e0fd8f53abce90cf044e1294f806393dff13a",
